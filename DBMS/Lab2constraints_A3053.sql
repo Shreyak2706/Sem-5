dfd5b17aac78b24be2005ca8e96b981404212ae9
Shreyak2706 <91607778+Shreyak2706@users.noreply.github.com>
2022-08-08 14:48:02 +0530

    Update Lab2constraints_A3053.sql

--- DBMS/Lab2constraints_A3053.sql
+++ DBMS/Lab2constraints_A3053.sql
@@ -87,25 +87,7 @@ mysql>
 mysql> create table Voters_2017(Voting_id varchar(10) PRIMARY KEY, Name char(20), Gender char(10), Age int CHECK(Age>=18), Region varchar(20) Default("Shivaji Nagar"));
 Query OK, 0 rows affected (0.04 sec)
 
-ubuntu@ubuntu-OptiPlex-5000:~$ mysql -u root -p
-Enter password: 
-Welcome to the MySQL monitor.  Commands end with ; or \g.
-Your MySQL connection id is 8
-Server version: 8.0.30-0ubuntu0.22.04.1 (Ubuntu)
 
-Copyright (c) 2000, 2022, Oracle and/or its affiliates.
-
-Oracle is a registered trademark of Oracle Corporation and/or its
-affiliates. Other names may be trademarks of their respective
-owners.
-
-Type 'help;' or '\h' for help. Type '\c' to clear the current input statement.
-
-mysql> use voting_2022
-Reading table information for completion of table and column names
-You can turn off this feature to get a quicker startup with -A
-
-Database changed
 mysql> create view voting as select Name, Region from voting_2022;
 ERROR 1146 (42S02): Table 'voting_2022.voting_2022' doesn't exist
 mysql> create view voting as select Name, Region from voters_2022;
