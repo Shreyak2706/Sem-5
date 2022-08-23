1e1bf6d0e6bcd5f36f0a1850bfda0295b10aed66
Shreyak2706 <91607778+Shreyak2706@users.noreply.github.com>
2022-08-23 15:21:22 +0530

    Update Lab5_Joins_A3053.sql

--- DBMS/Lab5_Joins_A3053.sql
+++ DBMS/Lab5_Joins_A3053.sql
@@ -210,6 +210,138 @@ mysql> SELECT customers.CustomerName, customers.ContactNo, orders.OrderID
 4 rows in set (0.00 sec)
 
 mysql> 
+mysql> SELECT customers.CustomerName, orders.OrderID
+    -> FROM customers
+    -> CROSS JOIN orders;
++--------------+---------+
+| CustomerName | OrderID |
++--------------+---------+
+| John Jacob   |   10311 |
+| John Jacob   |   10310 |
+| John Jacob   |   10309 |
+| John Jacob   |   10308 |
+| Riya Sharma  |   10311 |
+| Riya Sharma  |   10310 |
+| Riya Sharma  |   10309 |
+| Riya Sharma  |   10308 |
+| Anna Mary    |   10311 |
+| Anna Mary    |   10310 |
+| Anna Mary    |   10309 |
+| Anna Mary    |   10308 |
+| Mary Jane    |   10311 |
+| Mary Jane    |   10310 |
+| Mary Jane    |   10309 |
+| Mary Jane    |   10308 |
+| Kate R.      |   10311 |
+| Kate R.      |   10310 |
+| Kate R.      |   10309 |
+| Kate R.      |   10308 |
+| Rahul Verma  |   10311 |
+| Rahul Verma  |   10310 |
+| Rahul Verma  |   10309 |
+| Rahul Verma  |   10308 |
++--------------+---------+
+24 rows in set (0.00 sec)
+
+mysql> SELECT customers.CustomerName, orders.OrderDate FROM customers CROSS JOIN orders;
++--------------+------------+
+| CustomerName | OrderDate  |
++--------------+------------+
+| John Jacob   | 2022-08-24 |
+| John Jacob   | 2022-08-22 |
+| John Jacob   | 2022-08-23 |
+| John Jacob   | 2022-08-22 |
+| Riya Sharma  | 2022-08-24 |
+| Riya Sharma  | 2022-08-22 |
+| Riya Sharma  | 2022-08-23 |
+| Riya Sharma  | 2022-08-22 |
+| Anna Mary    | 2022-08-24 |
+| Anna Mary    | 2022-08-22 |
+| Anna Mary    | 2022-08-23 |
+| Anna Mary    | 2022-08-22 |
+| Mary Jane    | 2022-08-24 |
+| Mary Jane    | 2022-08-22 |
+| Mary Jane    | 2022-08-23 |
+| Mary Jane    | 2022-08-22 |
+| Kate R.      | 2022-08-24 |
+| Kate R.      | 2022-08-22 |
+| Kate R.      | 2022-08-23 |
+| Kate R.      | 2022-08-22 |
+| Rahul Verma  | 2022-08-24 |
+| Rahul Verma  | 2022-08-22 |
+| Rahul Verma  | 2022-08-23 |
+| Rahul Verma  | 2022-08-22 |
++--------------+------------+
+24 rows in set (0.00 sec)
+
+mysql> SELECT customers.Country, orders.OrderID FROM customers CROSS JOIN orders;
++---------+---------+
+| Country | OrderID |
++---------+---------+
+| Dubai   |   10311 |
+| Dubai   |   10310 |
+| Dubai   |   10309 |
+| Dubai   |   10308 |
+| India   |   10311 |
+| India   |   10310 |
+| India   |   10309 |
+| India   |   10308 |
+| USA     |   10311 |
+| USA     |   10310 |
+| USA     |   10309 |
+| USA     |   10308 |
+| Germany |   10311 |
+| Germany |   10310 |
+| Germany |   10309 |
+| Germany |   10308 |
+| London  |   10311 |
+| London  |   10310 |
+| London  |   10309 |
+| London  |   10308 |
+| India   |   10311 |
+| India   |   10310 |
+| India   |   10309 |
+| India   |   10308 |
++---------+---------+
+24 rows in set (0.00 sec)
+
+mysql> select * from orders;
++---------+------------+------------+
+| OrderID | CustomerID | OrderDate  |
++---------+------------+------------+
+|   10308 | 2          | 2022-08-22 |
+|   10309 | 37         | 2022-08-23 |
+|   10310 | 77         | 2022-08-22 |
+|   10311 | 28         | 2022-08-24 |
++---------+------------+------------+
+4 rows in set (0.00 sec)
+
+mysql> select * from customers;
++------------+--------------+-----------+---------+
+| CustomerID | CustomerName | ContactNo | Country |
++------------+--------------+-----------+---------+
+|          1 | John Jacob   | 374582827 | Dubai   |
+|          2 | Riya Sharma  | 938378292 | India   |
+|          3 | Anna Mary    | 83752728  | USA     |
+|          4 | Mary Jane    | 74848738  | Germany |
+|         28 | Kate R.      | 63774747  | London  |
+|         77 | Rahul Verma  | 74848738  | India   |
++------------+--------------+-----------+---------+
+6 rows in set (0.00 sec)
+
+mysql> SELECT customers.CustomerName, orders.OrderID
+    -> FROM customers
+    -> CROSS JOIN orders
+    -> WHERE customers.CustomerID=orders.CustomerID;
++--------------+---------+
+| CustomerName | OrderID |
++--------------+---------+
+| Riya Sharma  |   10308 |
+| Rahul Verma  |   10310 |
+| Kate R.      |   10311 |
++--------------+---------+
+3 rows in set (0.00 sec)
+
 
 
 
