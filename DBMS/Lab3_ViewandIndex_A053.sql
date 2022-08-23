89c52f7fc281db321eee57d2fdbc3c961463d10f
Shreyak2706 <91607778+Shreyak2706@users.noreply.github.com>
2022-08-23 16:36:46 +0530

    Update Lab3_ViewandIndex_A053.sql

--- DBMS/Lab3_ViewandIndex_A053.sql
+++ DBMS/Lab3_ViewandIndex_A053.sql
@@ -39,3 +39,67 @@ mysql> drop view delivery;
 Query OK, 0 rows affected (0.02 sec)
 mysql> select * from delivery;
 ERROR 1146 (42S02): Table 'couriers.delivery' doesn't exist
+
+
+
+
+
+
+
+mysql> select * from dunzo;
++---------+----------------+---------------+-----------------+
+| item_id | item_name      | address       | delivery_status |
++---------+----------------+---------------+-----------------+
+| MH00345 | Donut(MOD)     | Chinchwadgaon | Pending         |
+| MH48393 | Pizza(Dominos) | Aundh         | Delivered       |
+| MH94733 | Clothing(H&M)  | Aundh         | Delivered       |
+| MH00985 | Shoes(Puma)    | KP            | Pending         |
++---------+----------------+---------------+-----------------+
+4 rows in set (0.00 sec)
+
+mysql> 
+mysql> select * from delivery;
+ERROR 1146 (42S02): Table 'couriers.delivery' doesn't exist
+mysql> create view delivery as select item_id, delivery_status from dunzo;
+Query OK, 0 rows affected (0.01 sec)
+
+mysql> select * from delivery;
++---------+-----------------+
+| item_id | delivery_status |
++---------+-----------------+
+| MH00345 | Pending         |
+| MH48393 | Delivered       |
+| MH94733 | Delivered       |
+| MH00985 | Pending         |
++---------+-----------------+
+4 rows in set (0.00 sec)
+
+mysql> update delivery set delivery_status = "delivered" where item_id = "MH00345";
+Query OK, 1 row affected (0.03 sec)
+Rows matched: 1  Changed: 1  Warnings: 0
+
+mysql> select * from delivery;
++---------+-----------------+
+| item_id | delivery_status |
++---------+-----------------+
+| MH00345 | delivered       |
+| MH48393 | Delivered       |
+| MH94733 | Delivered       |
+| MH00985 | Pending         |
++---------+-----------------+
+4 rows in set (0.00 sec)
+
+mysql> select * from dunzo;
++---------+----------------+---------------+-----------------+
+| item_id | item_name      | address       | delivery_status |
++---------+----------------+---------------+-----------------+
+| MH00345 | Donut(MOD)     | Chinchwadgaon | delivered       |
+| MH48393 | Pizza(Dominos) | Aundh         | Delivered       |
+| MH94733 | Clothing(H&M)  | Aundh         | Delivered       |
+| MH00985 | Shoes(Puma)    | KP            | Pending         |
++---------+----------------+---------------+-----------------+
+4 rows in set (0.00 sec)
+
+mysql> 
+
+
